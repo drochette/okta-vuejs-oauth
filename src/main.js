acc652e461fb9499a6e6c50491395b148a959691
Damien Rochette <damien.rochette@aramisauto.com>
2023-01-10 16:44:43 +0100

    init authent

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {OktaAuth} from '@okta/okta-auth-js'
+import OktaVue from '@okta/okta-vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import sampleConfig from '@/config'
+
+const oktaAuth = new OktaAuth(sampleConfig.oidc)
+
+createApp(App)
+    .use(router)
+    .use(OktaVue, {oktaAuth})
+    .mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
+import {LoginCallback, navigationGuard} from '@okta/okta-vue'
+import ProfileComponent from '../src/components/Profile'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/login/callback',
+        component: LoginCallback
+    },
+    {
+        path: '/profile',
+        component: ProfileComponent,
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
+router.beforeEach(navigationGuard)
 
 export default router
